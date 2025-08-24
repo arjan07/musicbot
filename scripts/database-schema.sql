a1b2a23ed74d64abe6bf15ccf821108f11596fd5
Arjan <arjan_randhawa@outlook.com>
2025-08-24 16:16:48 +0100

    chore: update documentation (#2)

--- scripts/database-schema.sql
+++ scripts/database-schema.sql
@@ -120,10 +120,10 @@ DROP TABLE IF EXISTS `TrustedRole`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `TrustedRole` (
-                               `id` int NOT NULL AUTO_INCREMENT,
+                               `Id` int NOT NULL AUTO_INCREMENT,
                                `GuildId` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
                                `RoleId` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-                               PRIMARY KEY (`id`)
+                               PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
